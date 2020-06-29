33d5bbf9d866f09bb3fec317ad4552ffaaa05453
Cristian Sanchez <jazhiel.st@gmail.com>
2020-06-29 23:13:00 +0200

    Feat: Add Simple Vuejs App + Websockets

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,92 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="companies">
+      <div v-for="company in companies" :key="company.isin">
+        <Company 
+          v-bind:name="company.name"
+          v-bind:price="company.price"
+          v-bind:isin="company.isin"
+          v-bind:isSubscribed="company.isSubscribed"
+          @onSubscribe="subscribeToInstrument"
+          @onUnsubscribe="unsubscribeFromInstrument"
+        />
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Vue from 'vue';
+import Company from './components/Company';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Company
+  },
+  data(){
+    return {
+      companies: [{
+        name: 'Google Inc',
+        isin: 'US38259P5089',
+        isSubscribed : false,
+        price: null
+      }, {
+        name: 'Apple Inc',
+        isin: 'US0378331005',
+        isSubscribed : false,
+        price: null
+      }, {
+        name: 'Netflix Inc',
+        isin: 'US64110L1061',
+        isSubscribed : false,
+        price: null
+      }]
+    }
+  },
+  methods: {
+    updateCompanyFromState(isin, newState){
+      const index = this.companies.findIndex(company => company.isin === isin);
+      const current = this.companies[index];
+
+      Vue.set(this.companies, index, { ...current, ...newState});
+
+    },
+    subscribeToInstrument(isin){
+      
+      const message = {
+        subscribe: isin
+      };
+
+      this.connection.send(JSON.stringify(message));
+      this.updateCompanyFromState(isin, { isSubscribed: true });
+    },
+    unsubscribeFromInstrument(isin){
+      const message = {
+        unsubscribe: isin
+      };
+
+      this.connection.send(JSON.stringify(message));
+      this.updateCompanyFromState(isin, { isSubscribed: false });
+    },
+    
+  },
+  created(){
+    this.connection = new WebSocket('ws://159.89.15.214:8080/');
+
+    this.connection.onclose = () => {
+      // show some user feedback
+    } 
+
+    this.connection.onmessage = () => {
+      const { data } = event;
+      const { isin, price } = JSON.parse(data);
+
+      const priceRoundedToTwoDecimals = Math.round((price + Number.EPSILON) * 100) / 100
+
+      this.updateCompanyFromState(isin, { price: priceRoundedToTwoDecimals })
+    }
+
   }
 }
 </script>
---
+++ src/components/Company.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="company">
+    <button @click="subscribe(isin)" :disabled="isSubscribed === true">subscribe</button>
+    <button @click="unsubscribe(isin)" :disabled="isSubscribed !== true">unsubscribe</button>
+    <span>{{ name }} </span>
+    <span>Price: <strong>{{ price }}</strong></span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Company',
+  props: {
+    name: String,
+    price: Number,
+    isin: String,
+    isSubscribed: Boolean,
+    onSubscribe: Function,
+    onUnsubscribe: Function,
+  },
+  methods: {
+    subscribe(isin){
+      this.$emit('onSubscribe', isin);
+    },
+    unsubscribe(isin){
+      this.$emit('onUnsubscribe', isin);
+    }
+  }
+}
+</script>
+
+<style scoped>
+.company {
+  margin: 10px auto;
+}
+</style>
